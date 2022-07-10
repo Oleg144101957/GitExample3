c447fa1704c0c23b0488d113c2d9a2a7e9a4bbc2
Oleg144101957 <vishnevskiyoleg@gmail.com>
2022-07-10 12:25:52 +0300

    Adding counter

--- app/src/main/java/com/vishnevskiypro/gitexample3/MainActivity.kt
+++ app/src/main/java/com/vishnevskiypro/gitexample3/MainActivity.kt
@@ -2,12 +2,23 @@ package com.vishnevskiypro.gitexample3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vishnevskiypro.gitexample3.databinding.ActivityMainBinding
+
+var counter = 0
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
-        //my fours commit
+        binding.buttonCount.setOnClickListener {
+            counter++
+            binding.counterField.text = counter.toString()
+        }
+
+        //my fifth commit + new Feature
     }
 }

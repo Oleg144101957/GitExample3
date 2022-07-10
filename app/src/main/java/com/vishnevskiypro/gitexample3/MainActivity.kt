47f92934ad55e0cf12e6a5eca2fa7c7a9a0d24f3
Oleg144101957 <vishnevskiyoleg@gmail.com>
2022-07-10 12:04:42 +0300

    my third commit

--- app/src/main/java/com/vishnevskiypro/gitexample3/MainActivity.kt
+++ app/src/main/java/com/vishnevskiypro/gitexample3/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //my second commit
+        //my third commit
     }
 }
